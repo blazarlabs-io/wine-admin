d4afb3d811a7af1c98961b3db004ae504bba54aa
technoshaman <fitosegrera@gmail.com>
2024-05-27 18:46:58 -0500

    Update admin.ts

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -4,20 +4,15 @@ import { getFirestore } from "firebase-admin/firestore";
 import { getFunctions } from "firebase-admin/functions";
 
 // const serviceAccountKey = require("../../../serviceAccountKey.json");
-
+const serviceAccountKey = JSON.parse(
+  process.env.NEXT_PUBLIC_FIREBASE_SERVICE_ACCOUNT as string
+);
 let adminApp;
 
 // initialize admin firebase only once
 if (!getApps().length) {
   adminApp = initializeApp({
-    credential: cert({
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.NEXT_PUBLIC_FIREBASE_SERVICE_ACCOUNT?.replace(
-        /\\n/g,
-        "\n"
-      ),
-    }),
+    credential: cert(serviceAccountKey),
   });
 }
 
